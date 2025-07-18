56ffe6744bece4861e99ec8b8ceed03abf083ef4
Taofeek Lawal <taofeeklwl@gmail.com>
2025-07-18 20:05:16 +0100

    Recursion ib list

--- todo-app/src/FileExplorer.jsx
+++ todo-app/src/FileExplorer.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import json from "./data.json"
+
+
+
+// render list of object
+const List = ({list}) =>{
+
+    return(
+    <div className='container'>
+     {
+        list.map(node => (
+            <div key = {node.id}>
+                <span>{node.name}</span>
+
+                {/* Recursion  */}
+                
+                {node.children && <List list = {node.children}/>}
+                
+            </div>
+        ))
+     }
+
+    </div>
+    )
+}
+
 
 const FileExplorer = () => {
+
+    const  [data , setData] = useState([json])
   return (
 
     <div className='App'>
+        <h1>File / Folder Explorer</h1>
+        <List list = {data} />
+
+        
         
 
     </div>
